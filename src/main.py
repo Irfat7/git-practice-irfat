df98ef89bc6d786453937be15aef5961890560f4
Irfat7 <imirfat10@gmail.com>
2026-02-27 21:11:04 +0600

    Add basic error handling for division by zero

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from datetime import date
-from utils import add, subtract, multiply
+from utils import add, subtract, multiply, divide
 
 print("Irfat")
 print(date.today())
 print(add(1,2))
 print(subtract(1,2))
 print(multiply(2,2))
+print(divide(2,0))
